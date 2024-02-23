db5a63143b8d7adbe00d4ed49963cf52376f46f8
koalaphant <andrew.wardjones@icloud.com>
2024-02-23 08:29:10 +0000

    completed task 11 GET /api/articles (topic query

--- model/articles.models.js
+++ model/articles.models.js
@@ -57,32 +57,6 @@ function selectAllArticles(topic) {
   });
 }
 
-// function selectAllArticles(topic) {
-//   return db
-//     .query(
-//       `SELECT
-//   articles.author,
-//   articles.title,
-//   articles.article_id,
-//   articles.topic,
-//   articles.created_at,
-//   articles.votes,
-//   articles.article_img_url,
-//   COUNT(comments.comment_id)::integer AS comment_count
-// FROM
-//   articles
-// LEFT JOIN
-//   comments ON articles.article_id = comments.article_id
-// GROUP BY
-//   articles.article_id
-// ORDER BY
-//   articles.created_at DESC`
-//     )
-//     .then((result) => {
-//       return result.rows;
-//     });
-// }
-
 function selectCommentsByArticleId(articleId) {
   const queryString = format(
     "SELECT comment_id, votes, created_at, author, body, article_id FROM comments WHERE article_id = %L ORDER BY created_at DESC",
